e5c356e39455ccd02bee6d696bb25584bb59c2b6
Malik McDougald <121184781+Malikp33@users.noreply.github.com>
2023-02-23 23:30:47 +0000

    Finishing everything off

--- main.py
+++ main.py
@@ -64,17 +64,47 @@ def return_Exchange(amount):
 print('Hello, what are you here for?')
 print(options)
 opt = input('')
-global total 
-
-def exchange(newtotal):
-  global total
-  if opt == '1':
-    if newtotal == total or newtotal < total:
-      return 'Those items can be exchanged!'
+print()
+def new_tot():
+  pants = 0
+  shirts = 0
+  shorts = 0
+  hoodies = 0
+  jackets = 0 
+  print(clothes)
     
-    elif newtotal > total:
-      return 'That exchange cannot be done '
+  for i in range(amount):
+    exch = input('Choose your new items. \n')
       
+    if exch == '1':
+      pants += 1
+      
+    elif exch == '2':
+      shirts += 1
+      
+    elif exch == '3':
+      shorts += 1
+  
+    elif exch == '4':
+      hoodies += 1
+      
+    elif exch == '5':
+      jackets += 1
+    pant = 20 * pants
+    shirt = 30 * shirts
+    short = 25 * shorts
+    hood = 35 * hoodies
+    jack = 35 * jackets
+    newtotal = pant + shirt + short + hood + jack
+  return f'Your new total is: ${newtotal}'
+
+def exchange():
+  ask = input('Are these items more than your last items? (Yes/No): \n')
+  if ask == 'Yes' or ask == 'yes':
+    return 'These items cannot be exchanged.'
+  elif ask == 'no' or ask == 'No':
+    return 'These items can be exchanged'
+        
 # making responses to the users first option
 while opt != 1 or opt != 2:
   if opt == '1':
@@ -84,6 +114,7 @@ while opt != 1 or opt != 2:
   
   elif opt == '2':
     amount = int(input('How many items do you want to return?:'))
+    print()
     print(return_Exchange(amount))
     break
   
@@ -94,43 +125,32 @@ while opt != 1 or opt != 2:
     print(options)
     opt = input('')
 
+print()
+
+def opt4():
+  list = ['1.Cash back', '2. Store points']
+  print(list)
+  ask2 = input('Which would you like to do? (1 or 2): \n')
+  if ask2 == '1':
+    return 'Ok you will be receiving the money soon.'
+  elif ask2 == '2':
+    return 'Nice, see you soon.'
+    
 
 if opt == '1':
   ask = input('How many weeks have you had these items?: \n')
   if ask > '2':
-    print('You cannot return or exchange these items. Apologies')
+    print('You cannot exchange these items. Apologies')
   else:
-    pants = 0
-    shirts = 0
-    shorts = 0
-    hoodies = 0
-    jackets = 0 
-    print(clothes)
-    
-    for i in range(amount):
-      exch = input('Choose your new items. \n')
-      
-      if exch == '1':
-        pants += 1
-      
-      elif exch == '2':
-        shirts += 1
-      
-      elif exch == '3':
-        shorts += 1
-  
-      elif exch == '4':
-        hoodies += 1
-      
-      elif exch == '5':
-        jackets += 1
-    pant = 20 * pants
-    shirt = 30 * shirts
-    short = 25 * shorts
-    hood = 35 * hoodies
-    jack = 35 * jackets
-    newtotal = pant + shirt + short + hood + jack
-    print(exchange(newtotal))
+    print('Alright')
+    print()
+    print(new_tot())
+    print()
+    print(exchange())
 elif opt == '2':
+  ask = input('How many weeks have you had these items?: \n')
+  if ask > '2':
+    print('You cannot return these items. Sorry')
+  else:
+    opt4()
   
-  print(exchange(newtotal))
